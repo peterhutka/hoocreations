e2d2a90097b3f234747393b344a2eb5eaa1a7120
peterhutka <peter.hutka2@gmail.com>
2021-10-20 20:39:44 +0200

    test

--- src/Components/PowerLines/HomePowerLines.tsx
+++ src/Components/PowerLines/HomePowerLines.tsx
@@ -220,6 +220,7 @@ export default function HomePowerLines() {
                     gameState={gameState}
                     setGameState={setGameState}
                 /> }
+                
         </div>
     )
 }
