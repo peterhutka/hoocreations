20ecaacdd8e2015e142857146df53ecb15a42271
peterhutka <peter.hutka2@gmail.com>
2021-11-03 06:43:02 +0100

    clening consolelogs, env

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import React, {useContext} from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import HomePage from './Pages/HomePage';
-import AdminPage from './Pages/AdminPage';
-import Login from './Pages/Login';
-import Profile from './Pages/Profile';
 import "./main.css"
 import {myContext} from './Components/Context';
-import Register from './Pages/Register';
-import {PrivateRoute} from './Components/PrivateRoute';
-import ProtectedTest2 from './Components/ProtectedTest2';
 import PowerLines from './Components/PowerLines/PowerLines';
-import OnlyPublicRoute  from './Components/OnlyPublicRoute';
 import Portfolio from './Components/Portfolio/Portfolio';
 
 function App() {
