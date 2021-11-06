b3f800ea4594bd6405fd742120fee0a580b5f7c5
peterhutka <peter.hutka2@gmail.com>
2021-11-06 11:36:04 +0100

    debugin auth

--- src/Components/PowerLines/functions/PowerlinesLogin.tsx
+++ src/Components/PowerLines/functions/PowerlinesLogin.tsx
@@ -26,6 +26,7 @@ const Login = (props: any) => {
         }, {
             withCredentials: true
         }).then((res : AxiosResponse) => {
+            console.log("login response" , res.data)
             if (res.data === "success"){
                 updateUser({
                     from: "/login",
